a31a5d590d56316699ee5a67eaff9a756608f8d6
Shivang Kaushik <kaushik.shivangcoc@gmail.com>
2024-04-09 02:46:27 +0530

    tweaking mongo connect-2

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const dbUrl = process.env.DB_URL;
 //process.env.DB_URL;
 //mongodb://127.0.0.1:27017/yelp-camp
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp") 
+mongoose.connect(dbUrl) 
   .then(()=>{
     console.log("Mongo Connection Open")
   })
